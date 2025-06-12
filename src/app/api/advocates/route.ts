29d85f120b4b652123b10928f919c08f3231a1b9
Karmbir Chima <karmbirchima@gmail.com>
2025-06-11 21:03:37 -0700

    feat: fetching data from db

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,14 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
-
-  return Response.json({ data });
+    try {
+        const data = await db.select().from(advocates);
+        return Response.json({ data });
+    } catch (error) {
+        return Response.json(
+            { error: "Failed to fetch advocates." },
+            { status: 500 }
+        );
+    }
 }
